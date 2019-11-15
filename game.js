f96bff6705a4e0382c57e766e6dbe4d942a7548d
Le Hoang Long (FHM.EMU) <LongLH19@fsoft.com.vn>
2019-11-15 15:41:04 +0700

    initial project

---
+++ game.js
@@ -0,0 +1,103 @@
+// SELECT CANVAS
+const cvs = document.getElementById("bird");
+const ctx = cvs.getContext("2d");
+
+
+// GAME VAR AND  CONSTS
+let frames = 0;
+const sprite = new Image();
+sprite.src = "./img/sprite.png"
+
+const bg = {
+    sX: 0,
+    sY: 0,
+    w: 275,
+    h: 226,
+    x: 0,
+    y:cvs.height - 226,
+
+    draw: function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+}
+
+const fg = {
+    sX: 276,
+    sY: 0,
+    w: 224,
+    h: 112,
+    x: 0,
+    y:cvs.height - 112,
+
+    draw: function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    }
+}
+
+const bird = {
+    animation: [
+        {sX: 276, sY: 112},
+        {sX: 276, sY: 139},
+        {sX: 276, sY: 164},
+        {sX: 276, sY: 139}
+    ],
+    x: 50,
+    y: 150,
+    w: 34,
+    h: 26,
+    frame: 0,
+    draw: function() {
+        let bird = this.animation[this.frame];
+        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, this.x - this.w/2, this.y - this.h/2, this.w, this.h);
+    },
+    resetFrame: function() {
+        this.frame = 0;
+    }
+}
+
+const getReady = {
+    sX:0,
+    sY: 228,
+    w: 173,
+    h: 152,
+    x: cvs.width/2 - 173/2,
+    y: 80,
+
+    draw: function() {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+    }
+}
+
+// DRAW
+const draw = () => {
+    ctx.fillStyle = "#70c5ce";
+    ctx.fillRect(0, 0, cvs.width, cvs.height);
+    bg.draw();
+    fg.draw();
+    getReady.draw();
+    if( bird.frame === 3 ){
+        bird.resetFrame();
+    }else{
+        bird.frame++;
+    }
+    console.log(bird.frame);
+    bird.draw();
+}
+
+// UPDATE
+const update = () => {
+    
+}
+
+//LOOP
+const loop = () => {
+    update();
+    draw();
+    frames++;
+
+    requestAnimationFrame(loop);
+}
+
+loop();
